0094636cd98968537f9ebd2373bee220c7232fa1
mnola233 <mnola233@asu.edu>
2025-05-31 17:21:03 -0700

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/jira_dashboard_backend/JiraDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jira_dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
